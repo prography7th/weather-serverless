c3263c92815b52079acfa74118363a86698bb456
leesky <leehaneul0623@gmail.com>
2022-07-10 21:32:45 +0900

    feat: implements base caching line

--- src/forecast/forecast.module.ts
+++ src/forecast/forecast.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ContentsModule } from '../contents/contents.module';
 import { ForecastService } from './forecast.service';
+import { WeatherBaseHelper } from './weatherBase.helper';
 @Module({
   imports: [ContentsModule],
-  providers: [ForecastService],
+  providers: [ForecastService, WeatherBaseHelper],
 })
 export class ForecastModule {}
